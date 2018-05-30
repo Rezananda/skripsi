ff86a4c5e026eb83e18fa2d73dea791388c1e431
tutiwardani <tutiwardani@student.ub.ac.id>
2018-05-30 12:09:09 +0700

    Skripsi tuti

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
 from functools import wraps
-from flask import jsonify, request, Flask, make_response
+from flask import *
 import jwt
+import httplib2
 from models import User
 from controllers.app_core import app, db
 from pymongo import MongoClient
@@ -28,10 +29,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-client = MongoClient('10.34.216.102')
+#client = MongoClient('10.34.216.102')
+client = MongoClient('159.65.1.111', 29027,
+                        username='mongostorage',
+                        password='iotdatastr',
+                        authSource='admin',
+                        authMechanism='SCRAM-SHA-1')
 
 
-def validate_token(request):
+def validate_token(request,is_admin=False):
 
     auth_header = request.headers.get('Authorization')
 
@@ -46,26 +52,31 @@ def validate_token(request):
 
         if not user:
             return {'message': 'User not found'}
+        if is_admin and not user.is_admin:
+            return {'messgage': 'Not admin token'}
 
         return {'user': user.as_dict()}
 
     except Exception as error:
         return {'message': 'Token is invalid'}
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        res = validate_token(request)
-        if not res.get('user'):
-            return jsonify(res.get('message')), 401
-        return f(res.get('user'), *args, **kwargs)
-    return decorated
+def token_required(is_admin=False):
+    def token_required_inner(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            res = validate_token(request, is_admin)
+            if not res.get('user'):
+                return jsonify(res.get('message')), 401
+            return f(res.get('user'), *args, **kwargs)
+        return decorated
+    return token_required_inner
 
 
-"""@app.route("/")
-def root():
-    return jsonify({'message': 'API Root'})
-"""
+@app.route("/")
+def index():
+    return render_template('Login.html')
+
+
 """@app.route("/current-user")
 def api_get_current_user():
     auth_header = request.headers.get('Authorization')
@@ -89,33 +100,46 @@ def api_get_current_user():
     except Exception as error:
         return jsonify({'message': 'Token is invalid'}), 401
 """
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["POST", "GET"])
 def api_login():
-    try:
-        req = request.get_json(silent=True)
-        if not req or not req.get('username') or not req.get('password'):
-            return jsonify({
-                'message': 'No login data found'
-            })
-        user = User.query.filter_by(username=req.get('username')).first()
+    if request.method == "POST":
+        try:
+            #req = request.get_json(silent=True)
+            if  not request.form['username'] or not request.form['password']:
+                return jsonify({
+                    'message': 'No login data found'
+                })
+            user = User.query.filter_by(username=request.form['username']).first()
 
-        if user and user.check_password(req.get('password')):
-            token_data = {
-                'user_id': user.mac_address
-            }
+            if user and user.check_password(request.form['password']):
+                token_data = {
+                    'user_id': user.mac_address
+                }
 
-            token = jwt.encode(token_data, app.config['SECRET_KEY'])
-            return jsonify({'token': token.decode('UTF-8')})
+                token = jwt.encode(token_data, app.config['SECRET_KEY'])
+                session['token'] = token.decode('UTF-8')
+                return redirect('/home')
 
-        return jsonify({'message': 'invalid login'}), 401
+            return jsonify({'message': 'invalid login'}), 401
 
-    except Exception as error:
-        print(error)
-        return jsonify({'message': 'something went wrong'}), 400
+        except Exception as error:
+            print(error)
+            return jsonify({'message': 'something went wrong'}), 400
+    return render_template('Login.html')
+
+@app.route("/admin", methods=["POST", "GET"])
+def api_admin():
+    if request.method == "GET":
+        return render_template('Admin.html')
+    else:
+        data = User.query.all()
+        users = [user.as_dict() for user in data]
+        print (users)
+        return render_template('Admin.html', User=users)
 
 
 @app.route("/users")
-@token_required
+@token_required(is_admin=True)
 def api_get_users(current_user):
 
     data = User.query.all()
@@ -131,30 +155,50 @@ def api_get_users(current_user, user_id):
     users = [user.as_dict() for user in data]
     return jsonify(users)
 '''
-@app.route("/users", methods=['POST'])
+@app.route("/register", methods=["POST", "GET"])
 def api_create_users():
-    req = request.get_json(silent=True)
-    if not req:
-        return jsonify({
-            'message': 'No json data found'
-        })
-    try:
-        user = User(**req)
-        db.session.add(user)
-        db.session.commit()
+    if request.method == "POST":
+        #req = request.get_json(silent=True)
+        if not request.form['username'] or not request.form['password']:
+            return jsonify({
+                'message': 'No json data found'
+            })
+        try:
+            user = User(request.form['username'] , request.form['password'], request.form['mac_address'])
+            db.session.add(user)
+            db.session.commit()
 
-        return jsonify({
-            'message':'User with id {user.mac_address} created successfully',
-            'user': user.as_dict()
-        })
-    except Exception as error:
-        print(error)
-        return jsonify({
-            'message': 'Something went wrong'
-        }),400
+            return redirect('/login')
+
+        except Exception as error:
+            print(error)
+            return jsonify({
+                'message': 'Something went wrong'
+            }),400
+    return render_template('Register.html')
+
+@app.route('/home', methods=["POST", "GET"])
+def tampilan():
+    if request.method == "GET":
+        return render_template('Home.html')
+    else:
+        token = "Bearer " + request.form['Token']
+        tipedata = request.form['Tipedata']
+        conn = httplib2.HTTPConnectionWithTimeout("localhost:5001")
+        headers = {
+            "Authorization": token
+        }
+        conn.putrequest("GET", "/api/getdata" + tipedata)
+        conn.putheader("Authorization", token)
+        conn.endheaders()
+        response = conn.getresponse()
+        Data = response.read()
+        print (Data)
+        return render_template('Home.html', Data=Data)
+
 
 @app.route('/api/getdata',methods=['GET'])
-@token_required
+@token_required()
 def getdata(self):
     db_ = client.test
     data = []
@@ -162,17 +206,17 @@ def getdata(self):
         if request.args.get('topic')=="gambar":
             fs = gridfs.GridFS(db_)
             for data_ in fs.find():
-                data.append({"_id": str(data_._id), "filename": data_.filename, "md5":data_.md5})
+                data.append("https://iot.belajardisini.com:5001/api/getgambar/"+str(data_._id))
         else:
             for data_ in db_.testH__130.find({"topic": request.args.get('topic')},{"_id":0}):
                 data.append(data_)
     else:
         for data_ in db_.testH__130.find({}, {"_id": 0}):
             data.append(data_)
-    return json.dumps(data, indent=4, sort_keys=True)
+    return json.dumps(data, sort_keys=True)
 
 @app.route('/api/post', methods=['POST'])
-@token_required
+@token_required()
 def post(self):
     db = client.test
     fs = gridfs.GridFS(db)
